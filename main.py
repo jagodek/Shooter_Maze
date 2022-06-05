ab7fd60199b04b22ab5d7206bb17d070399e1435
jagodek <mchlgdk@gmail.com>
2022-06-05 22:23:25 +0200

    menu play

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import pygame as pg
+import pygame
+import os
 import numpy as np
 import math
+import sys
+from pathlib import Path
 from Player import Player
 from Bullet import Bullet
 from Wall import Wall
+from Button import Button
 from functions import *
 
 from game_params import WIN_WIDTH, WIN_HEIGHT, walking, bg, MAP_WIDTH, MAP_HEIGHT, WALLS_CORDS
 
+
+BG = pygame.image.load(Path("textures/assets/Background.png"))
+
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font(Path("textures/assets/font.ttf"), size)
 
 class Enemy(object):
     def __init__(self, x, y):
@@ -29,10 +39,10 @@ class Enemy(object):
         self.screen_x, self.screen_y = location_on_screen(
             player.x, player.y, self.x, self.y)
 
-        self.hitbox = pg.Rect(self.screen_x, self.screen_y, 0, 0).inflate(
+        self.hitbox = pygame.Rect(self.screen_x, self.screen_y, 0, 0).inflate(
             self.width / 2, self.height / 2)
 
-        pg.draw.rect(win, (255, 0, 0), (self.screen_x,
+        pygame.draw.rect(win, (255, 0, 0), (self.screen_x,
                      self.screen_y + 20, 20 * self.health, 20))
 
         # walks toward player
@@ -58,7 +68,7 @@ class Enemy(object):
                                      self.screen_x - aim_x) * 180 / math.pi + 90
         if self.animation_count + 1 >= 24:
             self.animation_count = 0
-        img_copy = pg.transform.rotate(
+        img_copy = pygame.transform.rotate(
             walking[self.animation_count // 3], self.angle)
         img_copy.set_colorkey((0, 0, 0))
         win.blit(img_copy,
@@ -87,9 +97,8 @@ class Backgrounds:
             win.blit(bg, (screen_x, screen_y))
 
 
-class MainMenu:
-    def __init__(self):
-        pass
+
+    
 
 
 def redrawGameWindow(win):
@@ -103,14 +112,14 @@ def redrawGameWindow(win):
     for enemy in enemies:
         enemy.main(win)
 
-    pg.draw.rect(win, (255, 0, 0), (player.screen_x, player.screen_y, 4, 4))
-    pg.draw.rect(win, (255, 0, 0), (60, 30, 20 * player.health, 20))
+    pygame.draw.rect(win, (255, 0, 0), (player.screen_x, player.screen_y, 4, 4))
+    pygame.draw.rect(win, (255, 0, 0), (60, 30, 20 * player.health, 20))
 
-    pg.display.update()
+    pygame.display.update()
 
 
 def handle_bullet(bullets_group, vel):
-    pg.sprite.groupcollide(bullets_group, walls_group, True, False)
+    pygame.sprite.groupcollide(bullets_group, walls_group, True, False)
     for bullet in bullets_group:
         if not (-MAP_WIDTH < bullet.rect.topleft[0] < MAP_WIDTH and -MAP_HEIGHT < bullet.rect.topleft[1] < MAP_HEIGHT):
             bullets_group.remove(bullet)
@@ -140,25 +149,25 @@ def detect_collision(walls_group, vel):
 
 
 def handle_keys(vel,right_block, left_block, down_block, up_block):
-    keys = pg.key.get_pressed()
+    keys = pygame.key.get_pressed()
     k_pressed = False
     player_move_vector = [0, 0]
-    if keys[pg.K_a] and player.x > vel and not left_block:
+    if keys[pygame.K_a] and player.x > vel and not left_block:
         k_pressed = True
         player_move_vector = np.add(player_move_vector, [-1, 0]).tolist()
         player.walking = True
 
-    if keys[pg.K_d] and player.x + 1 < MAP_WIDTH - vel - player.width / 2 and not right_block:
+    if keys[pygame.K_d] and player.x + 1 < MAP_WIDTH - vel - player.width / 2 and not right_block:
         k_pressed = True
         player_move_vector = np.add(player_move_vector, [1, 0]).tolist()
         player.walking = True
 
-    if keys[pg.K_w] and player.y - 1 > vel and not up_block:
+    if keys[pygame.K_w] and player.y - 1 > vel and not up_block:
         k_pressed = True
         player_move_vector = np.add(player_move_vector, [0, -1]).tolist()
         player.walking = True
 
-    if keys[pg.K_s] and player.y + 1 < MAP_HEIGHT - vel - player.width / 2 and not down_block:
+    if keys[pygame.K_s] and player.y + 1 < MAP_HEIGHT - vel - player.width / 2 and not down_block:
         k_pressed = True
         player_move_vector = np.add(player_move_vector, [0, 1]).tolist()
         player.walking = True
@@ -169,13 +178,9 @@ def handle_keys(vel,right_block, left_block, down_block, up_block):
     player.move(player_move_vector)
 
 
-def main():
-    pg.init()
-    win = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    pg.display.set_caption("Shooter_Maze")
-    exitgame = False
-
-    while not exitgame:
+def play_game(WIN):
+    WIN.fill("black")
+    while True:
         time_passed = clock.tick(50)
         print(time_passed)
         time_passed /= 1000
@@ -183,14 +188,16 @@ def main():
         vel = base_vel*time_passed
         player.set_speed(vel)
 
-        mouse_x, mouse_y = pg.mouse.get_pos()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
         map_mouse_x, map_mouse_y = location_on_map(
             player.x, player.y, mouse_x, mouse_y)  # position of mouse on map
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                exitgame = True
-            if event.type == pg.MOUSEBUTTONDOWN:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     bullets_group.add(
                         Bullet(player.x, player.y, map_mouse_x, map_mouse_y, player))
@@ -198,7 +205,7 @@ def main():
         
         # for i in playerBullets:
         #     point = i.x_screen, i.y_screen
-        #     if i.source != player and player.health > 0:
+            #     if i.source != player and player.health > 0:
         #         player.health -= 1
         #     for e in enemies:
         #         if i.source == player and e.hitbox.collidepoint(point):
@@ -215,20 +222,70 @@ def main():
         handle_keys(vel,right_block, left_block, down_block, up_block)
     
         
-        redrawGameWindow(win)
+        redrawGameWindow(WIN)
+        
+def main_menu(WIN,exitgame):
+    
+    WIN.fill("black")
+    while True:
+        WIN.blit(BG, (0,0))
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(50).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(WIN_WIDTH//2, WIN_HEIGHT//5))
+
+        PLAY_BUTTON = Button(image=pygame.image.load(Path("textures/assets/Play Rect.png")), pos=(WIN_WIDTH//2, WIN_HEIGHT//5*2), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load(Path("textures/assets/Options Rect.png")), pos=(WIN_WIDTH//2, WIN_HEIGHT//5*3), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load(Path("textures/assets/Quit Rect.png")), pos=(WIN_WIDTH//2, WIN_HEIGHT//5*4), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        WIN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(WIN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play_game(WIN)
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pass
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+        
+    
+
+def main():
+    pygame.init()
+    WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption("Shooter_Maze")
+    exitgame = False
+    main_menu(WIN,exitgame)
+    
+
+    
 
 
 if __name__ == '__main__':
     base_vel = 300
     player_size = (walking[0].get_width() + walking[0].get_height())/2
-    player_group = pg.sprite.Group()
+    player_group = pygame.sprite.Group()
     player = Player(player_size, WIN_WIDTH / 4 * 3, WIN_HEIGHT / 3 * 4)
     player_group.add(player)
-    clock = pg.time.Clock()
-    bullets_group = pg.sprite.Group()
+    clock = pygame.time.Clock()
+    bullets_group = pygame.sprite.Group()
     playerBullets = []
     enemies = []  # Enemy(10,10)]
-    walls_group = pg.sprite.Group()
+    walls_group = pygame.sprite.Group()
     for wall in WALLS_CORDS:
         walls_group.add(Wall(1, 1, wall[0], wall[1]))
 
